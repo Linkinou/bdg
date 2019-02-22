434b5eb359158048a13478af626a84afd4cbce96
Linkinou <cedric.merouani@gmail.com>
2019-02-22 16:52:35 -0700

    Adding timestampable

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -21,4 +21,5 @@ return [
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
